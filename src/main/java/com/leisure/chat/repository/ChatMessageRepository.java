4a1cb892f6c44aaf77b2879a0c8f2848e0799443
김남욱 <wsh6922@naver.com>
2026-09-14 22:51:56 +0900

    [feature/chat][Y2026-86][feat]: 채팅 리포지토리 추가 (방 조회/목록/삭제, 메시지 최근 이력)

--- src/main/java/com/leisure/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/leisure/chat/repository/ChatMessageRepository.java
@@ -1,7 +1,13 @@
 package com.leisure.chat.repository;
 
 import com.leisure.chat.domain.ChatMessage;
+import com.leisure.chat.domain.MessageStatus;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findTop6ByChatRoomIdAndMessageStatusOrderByCreatedAtDesc(Long chatRoomId, MessageStatus messageStatus);
 }
